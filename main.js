10411713b9a080aec48a7a989205515d40fad18f
bharatvitkar <79301656+bharatvitkar@users.noreply.github.com>
2022-08-20 09:35:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -34,3 +34,62 @@ function speak()
     var utterthis=new SpeechSynthesisUtterance(speak_data1+speak_data2);
     synth.speak(utterthis);
 }
+
+function check()
+{
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML=results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+        prediction_1=results[0].label;
+        prediction_2=results[1].label;
+        speak();
+        if(results[0].label=="happy")
+        {
+            document.getElementById("update_emoji").innerHTML="&#128522;";
+
+        }
+
+        if(results[0].label=="sad")
+        {
+            document.getElementById("update_emoji").innerHTML="&#128532;";
+            
+        }
+
+        if(results[0].label=="angry")
+        {
+            document.getElementById("update_emoji").innerHTML="&#128548;";
+            
+        }
+
+        if(results[1].label=="happy")
+        {
+            document.getElementById("update_emoji").innerHTML="&#128522;";
+
+        }
+
+        if(results[1].label=="sad")
+        {
+            document.getElementById("update_emoji").innerHTML="&#128532;";
+            
+        }
+
+        if(results[1].label=="angry")
+        {
+            document.getElementById("update_emoji").innerHTML="&#128548;";
+            
+        }
+
+    }
+}
